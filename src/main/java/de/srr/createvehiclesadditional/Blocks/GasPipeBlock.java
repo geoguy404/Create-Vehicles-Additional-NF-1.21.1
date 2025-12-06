215ad0380ff96e55f86b480360fdfdc4f7a2f65e
WhenLifeGivesYouOrangesMakeOrangeJuice <seb2022cool@gmail.com>
2025-12-06 23:57:04 +0100

    Updated Gas Pipes and added Connectable Pipes

--- src/main/java/de/srr/createvehiclesadditional/Blocks/GasPipeBlock.java
+++ src/main/java/de/srr/createvehiclesadditional/Blocks/GasPipeBlock.java
@@ -2,61 +2,108 @@ package de.srr.createvehiclesadditional.Blocks;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
 
 public class GasPipeBlock extends Block {
 
-    private static final VoxelShape SHAPE_NORTH = Block.box(4, 4, 0, 12, 12, 16);
-    private static final VoxelShape SHAPE_SOUTH = Block.box(4, 4, 0, 12, 12, 16);
-    private static final VoxelShape SHAPE_WEST  = Block.box(0, 4, 4, 16, 12, 12);
-    private static final VoxelShape SHAPE_EAST  = Block.box(0, 4, 4, 16, 12, 12);
+    public static final BooleanProperty NORTH = BooleanProperty.create("north");
+    public static final BooleanProperty SOUTH = BooleanProperty.create("south");
+    public static final BooleanProperty EAST  = BooleanProperty.create("east");
+    public static final BooleanProperty WEST  = BooleanProperty.create("west");
+    public static final BooleanProperty UP    = BooleanProperty.create("up");
+    public static final BooleanProperty DOWN  = BooleanProperty.create("down");
 
-
-    public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
+    private static final VoxelShape CORE  = Block.box(5, 5, 5, 11, 11, 11);
+    private static final VoxelShape NORTH_SHAPE = Block.box(5, 5, 0, 11, 11, 5);
+    private static final VoxelShape SOUTH_SHAPE = Block.box(5, 5, 11, 11, 11, 16);
+    private static final VoxelShape EAST_SHAPE  = Block.box(11, 5, 5, 16, 11, 11);
+    private static final VoxelShape WEST_SHAPE  = Block.box(0, 5, 5, 5, 11, 11);
+    private static final VoxelShape UP_SHAPE    = Block.box(5, 11, 5, 11, 16, 11);
+    private static final VoxelShape DOWN_SHAPE  = Block.box(5, 0, 5, 11, 5, 11);
 
     public GasPipeBlock(Properties properties) {
         super(properties.noOcclusion());
 
-        this.registerDefaultState(
-                this.stateDefinition.any().setValue(FACING, Direction.NORTH)
+        this.registerDefaultState(this.stateDefinition.any()
+                .setValue(NORTH, false)
+                .setValue(SOUTH, false)
+                .setValue(EAST,  false)
+                .setValue(WEST,  false)
+                .setValue(UP,    false)
+                .setValue(DOWN,  false)
         );
     }
 
-    @Override
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(FACING);
+    private boolean canConnectTo(BlockGetter level, BlockPos pos, Direction dir) {
+        BlockPos neighborPos = pos.relative(dir);
+        BlockState neighborState = level.getBlockState(neighborPos);
+        return neighborState.getBlock() instanceof GasPipeBlock;
     }
 
     @Override
     public BlockState getStateForPlacement(BlockPlaceContext ctx) {
+        BlockGetter level = ctx.getLevel();
+        BlockPos pos = ctx.getClickedPos();
+
         return this.defaultBlockState()
-                .setValue(FACING, ctx.getHorizontalDirection().getOpposite());
+                .setValue(NORTH, canConnectTo(level, pos, Direction.NORTH))
+                .setValue(SOUTH, canConnectTo(level, pos, Direction.SOUTH))
+                .setValue(EAST,  canConnectTo(level, pos, Direction.EAST))
+                .setValue(WEST,  canConnectTo(level, pos, Direction.WEST))
+                .setValue(UP,    canConnectTo(level, pos, Direction.UP))
+                .setValue(DOWN,  canConnectTo(level, pos, Direction.DOWN));
+    }
+
+    @Override
+    public @NotNull BlockState updateShape(@NotNull BlockState state, Direction direction,
+                                           BlockState neighbourState, @NotNull LevelAccessor level,
+                                           @NotNull BlockPos pos, @NotNull BlockPos neighbourPos) {
+
+        boolean connected = neighbourState.getBlock() instanceof GasPipeBlock;
+
+        return switch (direction) {
+            case NORTH -> state.setValue(NORTH, connected);
+            case SOUTH -> state.setValue(SOUTH, connected);
+            case EAST  -> state.setValue(EAST,  connected);
+            case WEST  -> state.setValue(WEST,  connected);
+            case UP    -> state.setValue(UP,    connected);
+            case DOWN  -> state.setValue(DOWN,  connected);
+        };
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(NORTH, SOUTH, EAST, WEST, UP, DOWN);
     }
 
     @Override
     public @NotNull VoxelShape getShape(BlockState state, @NotNull BlockGetter level,
                                         @NotNull BlockPos pos, @NotNull CollisionContext context) {
-        return switch (state.getValue(FACING)) {
-            case SOUTH -> SHAPE_SOUTH;
-            case WEST  -> SHAPE_WEST;
-            case EAST  -> SHAPE_EAST;
-            default -> SHAPE_NORTH;
-        };
+
+        VoxelShape shape = CORE;
+
+        if (state.getValue(NORTH)) shape = Shapes.or(shape, NORTH_SHAPE);
+        if (state.getValue(SOUTH)) shape = Shapes.or(shape, SOUTH_SHAPE);
+        if (state.getValue(EAST))  shape = Shapes.or(shape, EAST_SHAPE);
+        if (state.getValue(WEST))  shape = Shapes.or(shape, WEST_SHAPE);
+        if (state.getValue(UP))    shape = Shapes.or(shape, UP_SHAPE);
+        if (state.getValue(DOWN))  shape = Shapes.or(shape, DOWN_SHAPE);
+
+        return shape;
     }
 
     @Override
-    public boolean propagatesSkylightDown(@NotNull BlockState state, @NotNull BlockGetter level,
-                                          @NotNull BlockPos pos) {
+    public boolean propagatesSkylightDown(@NotNull BlockState state, @NotNull BlockGetter level, @NotNull BlockPos pos) {
         return true;
     }
 }
-
